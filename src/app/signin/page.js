170f97e274df0d8b2b55144b9f1a5dcd1629f8e4
Afariphi <agung.afr10@gmail.com>
2024-02-08 10:23:28 +0700

    first commit

---
+++ src/app/signin/page.js
@@ -0,0 +1,71 @@
+'use client'
+import React from "react";
+import signIn from "@/firebase/auth/signin";
+import { useRouter } from 'next/navigation'
+
+function Page() {
+  const [email, setEmail] = React.useState('')
+  const [password, setPassword] = React.useState('')
+  const router = useRouter()
+
+  const handleForm = async (event) => {
+    event.preventDefault()
+
+    const { result, error } = await signIn(email, password);
+
+    if (error) {
+      return console.log(error)
+    }
+
+    // else successful
+    console.log(result)
+    return router.push("/admin")
+  }
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="w-full max-w-xs">
+        <form onSubmit={handleForm} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <h1 className="text-3xl font-bold mb-6 text-black">Sign In</h1>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">
+              Email
+            </label>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              type="email"
+              name="email"
+              id="email"
+              placeholder="example@mail.com"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="mb-6">
+            <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">
+              Password
+            </label>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              type="password"
+              name="password"
+              id="password"
+              placeholder="password"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              type="submit"
+              className="w-full bg-blue-500 text-white font-semibold py-2 rounded"
+            >
+              Sign In
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Page;
